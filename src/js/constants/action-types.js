04298a9860870dd680048d4f7856d0d308e48e1d
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2019-03-23 14:19:50 +0200

    updates with new git versions

--- src/js/constants/action-types.js
+++ src/js/constants/action-types.js
@@ -1,4 +1,10 @@
 // src/js/constants/action-types.js
+export const baseUrl='https://ri7la-tech.com/rehlatech_sep/cruise/index.php/ar/mobile/'
+export const headers= {
+	'Content-Type': 'application/json',
+	'Accept': 'application/json',
+	'api':'9660c173205efca195c9c7d2d579b054'
+}
 
 export const LOGIN = "LOGIN";
 export const DO_LOGIN = "DO_LOGIN";
